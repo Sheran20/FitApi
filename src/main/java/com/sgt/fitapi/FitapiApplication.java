1816bc1ca0f525ac54fcb91695695bedd7ffccbf
Sheran <gt-sherantv@hotmail.com>
2025-09-26 18:46:57 -0400

    initial commit

---
+++ src/main/java/com/sgt/fitapi/FitapiApplication.java
@@ -0,0 +1,13 @@
+package com.sgt.fitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgt/fitapi/FitapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgt.fitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
