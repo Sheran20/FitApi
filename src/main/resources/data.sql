844c72acf997384070b1a973967941ab43a5f83f
Sheran <gt-sherantv@hotmail.com>
2025-11-30 19:50:10 -0500

    updating db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,19 @@
-MERGE INTO exercises (name, muscle_group, equipment, is_isometric, movement_type)
-    KEY(name) VALUES ('Bench Press', 'Chest', 'barbell', false, 'Push');
+INSERT INTO exercises (name, muscle_group, equipment, is_isometric, movement_type)
+VALUES ('Bench Press', 'Chest', 'barbell', false, 'Push')
+    ON CONFLICT (name) DO NOTHING;
 
-MERGE INTO exercises (name, muscle_group, equipment, is_isometric, movement_type)
-    KEY(name) VALUES ('Squat', 'Legs', 'barbell', false, 'Push');
+INSERT INTO exercises (name, muscle_group, equipment, is_isometric, movement_type)
+VALUES ('Squat', 'Legs', 'barbell', false, 'Push')
+    ON CONFLICT (name) DO NOTHING;
 
-MERGE INTO exercises (name, muscle_group, equipment, is_isometric, movement_type)
-    KEY(name) VALUES ('Deadlift', 'Back', 'barbell', false, 'Pull');
+INSERT INTO exercises (name, muscle_group, equipment, is_isometric, movement_type)
+VALUES ('Deadlift', 'Back', 'barbell', false, 'Pull')
+    ON CONFLICT (name) DO NOTHING;
 
-MERGE INTO exercises (name, muscle_group, equipment, is_isometric, movement_type)
-    KEY(name) VALUES ('Overhead Press', 'Shoulders', 'barbell', false, 'Push');
+INSERT INTO exercises (name, muscle_group, equipment, is_isometric, movement_type)
+VALUES ('Overhead Press', 'Shoulders', 'barbell', false, 'Push')
+    ON CONFLICT (name) DO NOTHING;
 
-MERGE INTO exercises (name, muscle_group, equipment, is_isometric, movement_type)
-    KEY(name) VALUES ('Plank', 'Core', 'bodyweight', true, 'Other');
+INSERT INTO exercises (name, muscle_group, equipment, is_isometric, movement_type)
+VALUES ('Plank', 'Core', 'bodyweight', true, 'Other')
+    ON CONFLICT (name) DO NOTHING;
